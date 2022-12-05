0b0b0035a1655eafe1c1fe0648db99e26a338998
Federico Zattera <fzattera@gmail.com>
2022-12-05 12:03:01 -0300

    NUEVA VERSION!!

--- js/funciones.js
+++ js/funciones.js
@@ -8,3 +8,9 @@ function myFunction() {
     }
   }
   
+
+  window.addEventListener('load', () => {
+    const contenedorLoader =  document.querySelector('.contenedor-loader')
+    contenedorLoader.style.opacity = 0;
+    contenedorLoader.style.visibility = 'hidden'
+})
